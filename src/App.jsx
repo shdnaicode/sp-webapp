4e0a511eeba61fd5dcfed2cd176f53512307b805
fiddleswe <smartsoodprasert69@gmail.com>
2025-09-12 18:28:00 +0700

    Update: Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import { Route, Router, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-      </div>
+      <Routes>
+        <Route path="/" element={""} />
+      </Routes>
     </>
   )
 }
