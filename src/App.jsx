3eb819e81b309fd8bd992de275389b38a109989f
fiddleswe <smartsoodprasert69@gmail.com>
2025-09-15 23:49:13 +0700

    Fix: Add nested routes for Home.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Register from "./pages/register";
 import Login from "./pages/login";
 import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import Progress from "./pages/progress";
+import Project from "./pages/project";
+import "./App.css"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={< Home/>} />
-      <Route path="/Register" element={< Register/>} />
-      <Route path="/Login" element={< Login/>} />
+      <Route path="/" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+
+      <Route path="/home" element={<Home />}>
+        <Route index element={<Dashboard />} />
+        <Route path="progress" element={<Progress />} />
+        <Route path="project" element={<Project />} />
+      </Route>
     </Routes>
   );
 }
